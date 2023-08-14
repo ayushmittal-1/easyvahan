8e6290d9ecdc9839bdf12e5d9c2cc83cd382272a
Ayush Mittal <ayushmittalbkn@gmail.com>
2023-08-14 17:48:58 +0530

    Landing Page and Initialisation

---
+++ lib/mainpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class mainpage extends StatefulWidget {
+  const mainpage({Key? key}) : super(key: key);
+
+  @override
+  State<mainpage> createState() => _mainpageState();
+}
+
+class _mainpageState extends State<mainpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
